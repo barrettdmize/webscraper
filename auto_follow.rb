4bd3ea65d7cf39c155c471d35ed34f4570176c66
Barrett Mize <barrettdmize@gmail.com>
2018-01-12 16:40:13 -0800

    added the ability to navigate to the correct fields and click the login button

--- auto_follow.rb
+++ auto_follow.rb
@@ -6,5 +6,16 @@ password = "password"
 user = "justinbieber"
 
 # Open Browser, Navigate to login page
-browser = Watir::Browswer.new :chrome
-browser.goto "instagram.com/accounts/login"
+browser = Watir::Browser.new :chrome
+browser.goto "instagram.com/accounts/login/"
+
+# Navigate to Username and Password fields, inject info
+puts "Logging in..."
+browswer.text_field(:name => "username").set "#{username}"
+browswer.text_field(:name => "password").set "#{password}"
+
+# Click Login button
+browser.button(:class => '_qv64e _gexxb _4tgw8 _njrw0').Click
+
+
+sleep(360)
