87156f8c23e9447562006ed667ddf2a81a7e01e5
Barrett Mize <barrettdmize@gmail.com>
2018-01-19 16:36:10 -0800

    got chrome driver working and the bot to log in successfully

--- auto_follow.rb
+++ auto_follow.rb
@@ -1,8 +1,8 @@
 require 'watir'
 require_relative 'credentials'
 
-username = "username"
-password = "password"
+username = "bear_duke"
+password = "Pa55w3rd..."
 user = "justinbieber"
 
 # Open Browser, Navigate to login page
@@ -11,8 +11,8 @@ browser.goto "instagram.com/accounts/login/"
 
 # Navigate to Username and Password fields, inject info
 puts "Logging in..."
-browswer.text_field(:name => "username").set "#{username}"
-browswer.text_field(:name => "password").set "#{password}"
+browser.text_field(:name => "username").set "#{username}"
+browser.text_field(:name => "password").set "#{password}"
 
 # Click Login button
 browser.button(:class => '_qv64e _gexxb _4tgw8 _njrw0').click
