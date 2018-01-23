9a63604c4c9ad58f64fe43b8664312963f434d3d
Barrett Mize <barrettdmize@gmail.com>
2018-01-22 16:36:25 -0800

    changed this to work with a different site so that I could further my understanding of what is happening

--- auto_follow.rb
+++ auto_follow.rb
@@ -1,21 +1,21 @@
 require 'watir'
 require_relative 'credentials'
 
-username = "bear_duke"
+email = "bear.mize@bethel.com"
 password = "Pa55w3rd..."
-user = "justinbieber"
+# user = "justinbieber" commeting outcause this is only for insta
 
 # Open Browser, Navigate to login page
 browser = Watir::Browser.new :chrome
-browser.goto "instagram.com/accounts/login/"
+browser.goto "https://www.bethel.tv/login"
 
 # Navigate to Username and Password fields, inject info
 puts "Logging in..."
-browser.text_field(:name => "username").set "#{username}"
+browser.text_field(:name => "email").set "#{email}"
 browser.text_field(:name => "password").set "#{password}"
 
 # Click Login button
-browser.button(:class => '_qv64e _gexxb _4tgw8 _njrw0').click
+browser.button(:class => 'button pure-blue small').click
 
 
 sleep(360)
